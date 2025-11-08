bd18302b7c9d8fda6926144e164bc4fb81754a0c
Kavelin <dev.velp@gmail.com>
2025-11-08 00:21:26 -0500

    Open Graph Protocols for main app

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,19 @@
 <script>
-  import { onMount } from 'svelte'
-  import Layout from './components/Layout.svelte'
-  import Home from './pages/Home.svelte'
-  import AboutUs from './pages/AboutUs.svelte'
-  import Join from './pages/Join.svelte'
-  import Sponsorship from './pages/Sponsorship.svelte'
-  import NotFound from './pages/NotFound.svelte'
+  import { onMount } from "svelte";
+  import Layout from "./components/Layout.svelte";
+  import Home from "./pages/Home.svelte";
+  import AboutUs from "./pages/AboutUs.svelte";
+  import Join from "./pages/Join.svelte";
+  import Sponsorship from "./pages/Sponsorship.svelte";
+  import NotFound from "./pages/NotFound.svelte";
 
-  export let route = '/home'
+  export let route = "/home";
 
   function updateRoute() {
-    const p = location.pathname.replace(/\/+$/, '')
-    if (!p || p === '/' || p === '/index.html') {
-      route = '/home'
-      return
+    const p = location.pathname.replace(/\/+$/, "");
+    if (!p || p === "/" || p === "/index.html") {
+      route = "/home";
+      return;
     }
     // map to our simple routes
     route = p;
@@ -23,15 +23,15 @@
     updateRoute();
     console.log(route);
     navigate(route);
-    window.addEventListener('popstate', updateRoute)
-  })
+    window.addEventListener("popstate", updateRoute);
+  });
 
   let open = false;
 
   function navigate(path) {
-    history.pushState({}, '', path)
-    updateRoute()
-    window.scrollTo(0, 0)
+    history.pushState({}, "", path);
+    updateRoute();
+    window.scrollTo(0, 0);
     open = false;
   }
 
@@ -39,18 +39,39 @@
     open = !open;
   }
 </script>
+
 <svelte:head>
-   <title> {route.slice(1).replace(/\w\S*/g, t => t.charAt(0).toUpperCase() + t.substring(1).toLowerCase())}</title>
-</svelte:head>
+  <title>
+    {route
+      .slice(1)
+      .replace(
+        /\w\S*/g,
+        (t) => t.charAt(0).toUpperCase() + t.substring(1).toLowerCase()
+      )}</title
+  >
+  <meta property="og:title" content="Patriot Motorsports" />
+  <meta
+    property="og:description"
+    content="George Mason University's FSAE Team."
+  />
+  <meta
+    property="og:image"
+    content="https://patriotfsae.vercel.app/logo.jpg"
+  />
+  <meta
+    property="og:url"
+    content="https://patriotfsae.vercel.app/"
+  /></svelte:head
+>
 
 <Layout {navigate} {open} {hamburger} {route}>
-  {#if route === '/home'}
+  {#if route === "/home"}
     <Home />
-  {:else if route === '/about'}
+  {:else if route === "/about"}
     <AboutUs />
-  {:else if route === '/join'}
+  {:else if route === "/join"}
     <Join />
-  {:else if route === '/sponsorship'}
+  {:else if route === "/sponsorship"}
     <Sponsorship />
   {:else}
     <NotFound />
